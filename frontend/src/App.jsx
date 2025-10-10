756089bbb12e77b1c92560d63bdc885ed5d275de
GuiHenry17 <gui201629@gmail.com>
2025-10-09 23:57:37 -0300

    -

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Perfil from './pages/Perfil'
+import Home from './pages/Home'
 
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      <h1>Oiii</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path="/perfil" element={<Perfil />} />
+      </Routes>
+    </Router>
   )
 }
-
-export default App;
